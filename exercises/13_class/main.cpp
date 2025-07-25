606204eea53db42560063ae104cf3753df626dc4
hniii98 <752608928@qq.com>
2025-07-25 12:18:00 +0900

    Finish to ex18

--- exercises/13_class/main.cpp
+++ exercises/13_class/main.cpp
@@ -9,16 +9,18 @@
 // 因此必须提供构造器来初始化字段。
 // READ: 构造器 <https://zh.cppreference.com/w/cpp/language/constructor>
 class Fibonacci {
+public:
     size_t cache[16];
     int cached;
 
 public:
     // TODO: 实现构造器
     // Fibonacci()
+    Fibonacci() : cache({0, 1}), cached(2){};
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
+        for (; i >= cached; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
@@ -29,6 +31,7 @@ int main(int argc, char **argv) {
     // 现在类型拥有无参构造器，声明时会直接调用。
     // 这个写法不再是未定义行为了。
     Fibonacci fib;
+    std::cout << fib.cache[0] <<  fib.cache[1] << std::endl;
     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
     return 0;
